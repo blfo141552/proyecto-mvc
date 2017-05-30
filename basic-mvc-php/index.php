263a2d389871b510a36bbf4e7f6d3e7cbc275ca6
blfo141552 <blfo141552@upemor.edu.mx>
2017-05-30 00:01:04 -0500

    Add files via upload

--- basic-mvc-php/index.php
+++ basic-mvc-php/index.php
@@ -1,16 +1,47 @@
-<?php
-	include_once("controller/ProductController.php");
-	$products = new ProductController();
-?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Productos</title>
-	</head>
-	<body>
-	<?php
-		$products->index();
-	?>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>Pizza</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="  crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- my css file -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+  </head>
+  <body>
+    <!-- header -->
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Lobos pizza</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </header>
+    <div class="video-container">
+      <video class="video" src="./public/video.mp4" autoplay loop="">
+      </video>
+    </div>
+        <div class="video-container vertical-center">
+		<!-- FORMULARIO PARA MOSTRAR LOS PRODUCTOS REGISTRADOS -->
+		<div class="front absolute  col-xs-4"></div>
+    <div class="front absolute card col-xs-4">
+      <button onclick = "location='views/User.php'">Usuario</button>
+      <br>
+      <br>
+      <button onclick = "location='views/admin.php'">Administrador</button></div>
+    <div class="front absolute col-xs-4"></div>
+
+			</div>
+  </body>
 </html>
